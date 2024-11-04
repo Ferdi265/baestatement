651f25e54fd89374e8797911593d917ce3d162f8
Ferdinand Bachmann <ferdinand.bachmann@yrlf.at>
2024-11-04 01:13:15 +0100

    baestatement/format/dump: fix dump not generating parsable python

--- baestatement/format/dump.py
+++ baestatement/format/dump.py
@@ -1,5 +1,5 @@
 from ..parse import Statement, StatementLine, StatementSummary
-from .util import fmt_amount, fmt_date
+from .util import fmt_amount_repr, fmt_date_repr
 
 def dump_line(line: StatementLine) -> str:
     formatted = "        StatementLine(\n"
@@ -15,13 +15,13 @@ def dump_line(line: StatementLine) -> str:
             formatted += f"                {repr(text_line + '\n')}\n"
 
     formatted += f"            ),\n"
-    formatted += f"            amount={fmt_amount(line.amount)}, booking_date={fmt_date(line.booking_date)}, value_date={fmt_date(line.value_date)},\n"
+    formatted += f"            amount={fmt_amount_repr(line.amount)}, booking_date={fmt_date_repr(line.booking_date)}, value_date={fmt_date_repr(line.value_date)},\n"
     formatted += "        ),\n"
     return formatted
 
 def dump_summary(summary: StatementSummary) -> str:
     formatted = "    summary=StatementSummary(\n"
-    formatted += f"        date={fmt_date(summary.date)},\n"
+    formatted += f"        date={fmt_date_repr(summary.date)},\n"
     formatted += f"        sum_expenses={summary.sum_expenses:.2f}, sum_income={summary.sum_income:.2f},\n"
     formatted += f"        old_balance={summary.old_balance:.2f}, new_balance={summary.new_balance:.2f},\n"
     formatted += "    ),\n"

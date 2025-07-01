0d3deb2b4ad4f2482b9584a4387a2f379ad6fc57
Ferdinand Bachmann <ferdinand.bachmann@yrlf.at>
2025-07-01 21:14:09 +0200

    baestatement/cli/show: allow passing a directory

--- baestatement/cli/show.py
+++ baestatement/cli/show.py
@@ -1,4 +1,5 @@
 from argparse import Namespace as Args
+from glob import glob
 from baestatement.cli.util import create_default_argparser, add_default_options
 from baestatement.cli.util import parse_statement_from_pdf
 from baestatement.format import format_cli
@@ -10,9 +11,14 @@ def parse_args() -> Args:
 
 def main():
     args = parse_args()
-    stmt = parse_statement_from_pdf(args.pdf, args)
+    if args.pdf.is_dir():
+        pdfs = sorted(glob(str(args.pdf / "*.pdf")))
+    else:
+        pdfs = [args.pdf]
 
-    print(format_cli(stmt))
+    stmts = [parse_statement_from_pdf(pdf, args) for pdf in pdfs]
+    for stmt in stmts:
+        print(format_cli(stmt))
 
 if __name__ == '__main__':
     main()
